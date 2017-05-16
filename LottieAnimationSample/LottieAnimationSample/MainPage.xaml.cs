f7f771ac32a36b68db7c3de99d8efa2bdb2015de
Adam Pedley <adam.pedley@gmail.com>
2017-05-16 16:04:30 +1000

    Add project files.

---
+++ LottieAnimationSample/LottieAnimationSample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Lottie.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LottieAnimationSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			var animationView = new AnimationView()
+			{
+				
+			};
+
+		}
+	}
+}
